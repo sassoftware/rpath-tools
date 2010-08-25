3cf8e02cba3fe55e5a6dd18fcd567ad3c548df4e
Mihai Ibanescu <https://issues.rpath.com/>
2010-08-25 17:22:44 -0400

    Back to using https

--- rpath_tools/client/utils/client.py
+++ rpath_tools/client/utils/client.py
@@ -35,7 +35,7 @@ class RegistrationClient(Client):
 
     SUCCESS_CODES = [200, 201]
     PATH = '/api/inventory/systems/'
-    SCHEME = 'http'
+    SCHEME = 'https'
 
     def __init__(self, url):
         self.url = urlparse.urlunsplit([self.SCHEME, url, self.PATH, None, None])
