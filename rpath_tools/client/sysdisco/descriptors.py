5a98a36047d9c70fc280a03aa22a4b62e6695ac7
Brett Smith <https://issues.rpath.com/>
2012-07-20 17:12:32 -0400

    adding descriptors module

---
+++ rpath_tools/client/sysdisco/descriptors.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+from xml.etree import cElementTree as etree
+
+from conary import conarycfg
+from conary import conaryclient
+from rpath_tools.client.utils.config_descriptor_cache import ConfigDescriptorCache
+
+class Descriptors(object):
+    def __init__(self):
+        self.cfg = conarycfg.ConaryConfiguration(True)
+        self.client = conaryclient.ConaryClient(self.cfg)
+
+    def gather(self):
+        group = [ x for x in self.client.getUpdateItemList()
+                    if x[0].startswith('group-') and 
+                    x[0].endswith('-appliance') ][0]
+
+        desc = ConfigDescriptorCache(self.client.getDatabase()).getDescriptor(group)
+        desc.setDisplayName('ConfigurationDescriptor')
+        desc.addDescription('ConfigurationDescriptor')
+        return desc
+
+    def toxml(self):
+        desc = self.gather()
+        return  desc.toxml()
+
+if __name__ == '__main__':
+    import sys
+    from conary.lib import util
+    sys.excepthook = util.genExcepthook()
+
+    descriptors = Descriptors()
+    xml = etree.fromstring(descriptors.toxml())
+    print etree.tostring(xml)
+
