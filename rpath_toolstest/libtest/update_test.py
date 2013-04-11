8adf3ab8102327022e7f382687dfb3816d173855
Brett Smith <bc.smith@sas.com>
2013-04-11 11:30:05 -0400

    Add update_test for rpath_tools.lib.update RCE-1494

---
+++ rpath_toolstest/libtest/update_test.py
@@ -0,0 +1,93 @@
+#
+# Copyright (c) SAS Institute Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+
+import testbaserepo
+
+from rpath_tools.lib import stored_objects, update
+
+class UpdateTest(testbaserepo.TestCase):
+
+    jobFactory = stored_objects.ConcreteUpdateJobFactory
+
+    def setUp(self):
+        testbaserepo.TestCase.setUp(self)
+        self.openRepository()
+        for v in ["1", "2"]:
+            self.addComponent("foo:runtime", v)
+            self.addCollection("foo", v, [":runtime"])
+            self.addCollection("group-bar", v, [ "foo" ])
+
+        self.updatePkg(["group-bar=1"])
+
+        self.systemModelPath = os.path.join(self.workDir, "system-model")
+        file(self.systemModelPath, "w").write("install group-bar=2\n")
+
+    def newJob(self):
+        self.job = self.jobFactory(self.storagePath).new()
+        self.job.state = "New"
+        return self.job
+
+    def loadJob(self, jobId):
+        self.job = self.jobFactory(self.storagePath).load(self.sanitizeKey(jobId))
+        return self.job
+
+
+
+    def testSyncModelPreviewOperation(self):
+
+        self.job = self.newJob()
+        self.job.systemModel = file(self.systemModelPath).read()
+        operation = update.SyncModel()
+        preview = operation.preview(self.job)
+        self.job.content = preview
+        # need something to check there is a preview
+        if preview:
+            return True
+
+    def testSyncModelApplyOperation(self):
+        self.job = self.newJob()
+        self.job_test = self.loadJob(self.job.keyId)
+
+        operation = update.SyncModel()
+        preview = operation.apply(self.job_test)
+        self.job.content = preview
+
+        # need something to check there is a preview
+        if preview:
+            return True
+
+    def testFreezeUpdateJob(self):
+        pass
+
+    def testThawUpdateJob(self):
+        pass
+
+    def testReadSystemModel(self):
+        pass
+
+    def testUpdateModel(self):
+        pass
+
+    def testUpdateModelFromString(self):
+        pass
+
+    def testTopLevelItems(self):
+        pass
+
+
+
